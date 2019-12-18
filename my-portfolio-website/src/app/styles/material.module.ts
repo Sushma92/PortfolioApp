e153be8a78586903e2f30a1edeebcc157513cf36
Sushma92 <nuthanap1992@gmail.com>
2019-12-17 22:26:13 -0500

    setting matcardmodule

---
+++ my-portfolio-website/src/app/styles/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+
+@NgModule({
+    imports: [MatCardModule, MatDividerModule],
+    exports: [MatCardModule, MatDividerModule],
+    
+})
+
+export class MaterialModule{}
